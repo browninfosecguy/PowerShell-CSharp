9d2e823cc5e50f6fce70203469b1f7e01d17804e
Sonny <37906998+browninfosecguy@users.noreply.github.com>
2020-05-13 14:41:21 -0300

    Initial Commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+//In order for this to work you need to install the following Package Install-Package Microsoft.PowerShell.SDK
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Time for PowerShell magic using C#!");
+
+            PowerShell psPipeline = PowerShell.Create();
+
+            psPipeline.AddCommand("Get-ChildItem");
+
+            psPipeline.AddParameter("Path", "C:\\Users");
+
+            var pipelineOutput = psPipeline.Invoke();
+
+            Console.WriteLine("\r\nRaw pipeline output");
+
+            Console.WriteLine(pipelineOutput);
+
+            Console.WriteLine("\r\nList Each object seperately");
+
+            if (pipelineOutput.Count > 0)
+            {
+                foreach (var objects in pipelineOutput)
+                {
+                    Console.WriteLine(objects);
+                }
+
+            }
+
+        }
+    }
+}
+
